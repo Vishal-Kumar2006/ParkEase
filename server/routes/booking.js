455f7d3d7ef4e4567af184b702a875fe62f5b359
Vishal Kumar <vishalkumar438455@gmail.com>
2025-03-20 20:47:37 +0530

    Re-adding all files on Github

--- server/routes/booking.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {handleBooking, getBookingById} = require("../controllers/booking.js");
-
-router.post("/book", handleBooking);
-router.get("/:id", getBookingById);
-
-
-module.exports = router;
